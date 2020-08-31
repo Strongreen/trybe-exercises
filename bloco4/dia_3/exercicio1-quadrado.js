43dd5f212f31b0adb7853363a952a822406b86f1
Strongreen <strongreen@live.com>
2020-08-31 19:10:05 -0300

    Exercicio 1 correçao

--- bloco4/dia_3/exercicio1-quadrado.js
+++ bloco4/dia_3/exercicio1-quadrado.js
@@ -2,7 +2,7 @@ let n = 5;
 let asteriscos = "";
 
 for(let i=0; i <= n; i++){
-    if(asteriscos.length === 5){
+    if(asteriscos.length === n){
         for (let j = 0; j < n; j++){
             console.log(asteriscos);
         }
