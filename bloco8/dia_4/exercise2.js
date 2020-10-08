cc40934d39bd100b78690c416464a192d24be272
Strongreen <strongreen@live.com>
2020-10-08 19:18:25 -0300

    Exercicio 3 concluida

--- bloco8/dia_4/exercise2.js
+++ bloco8/dia_4/exercise2.js
@@ -6,8 +6,11 @@ const sum = (...numbers) => {
  return total
 }
 
+
+
 assert.equal(sum(), 0)
 assert.equal(sum(1), 1)
 assert.equal(sum(1, 2), 3)
 assert.equal(sum(1, 2, 3), 6)
 assert.equal(sum(1, 2, 3, 4), 10)
+
